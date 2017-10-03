b4c44959c2c850bd55ee66f6b6b12df1c579a4e6
Yusril Herlian Syah <yusrilhsyah@gmail.com>
2017-10-03 00:37:32 -0400

    Modify test data

--- Tests/Unit/ChartJsHelperTest.php
+++ Tests/Unit/ChartJsHelperTest.php
@@ -6,18 +6,18 @@ class ChartJsHelperTest extends PHPUnit_Framework_TestCase {
 
     public function testCreateChart() {
         $barChart = ChartjsHelper::createChart('bar');
+        $bubbleChart = ChartjsHelper::createChart('bubble');
         $doughnutChart = ChartjsHelper::createChart('doughnut');
         $lineChart = ChartjsHelper::createChart('line');
-        $mixedChart = ChartjsHelper::createChart('mixed');
         $pieChart = ChartjsHelper::createChart('pie');
         $polarChart = ChartjsHelper::createChart('polarArea');
         $radarChart = ChartjsHelper::createChart('radar');
+        $scatterChart = ChartjsHelper::createChart('scatter');
 
         // Test instance of each chart
         $this->assertInstanceOf('YusrilHs\\ChartjsHelper\\ChartJsLib', $barChart);
         $this->assertInstanceOf('YusrilHs\\ChartjsHelper\\ChartJsLib', $doughnutChart);
         $this->assertInstanceOf('YusrilHs\\ChartjsHelper\\ChartJsLib', $lineChart);
-        $this->assertInstanceOf('YusrilHs\\ChartjsHelper\\ChartJsLib', $mixedChart);
         $this->assertInstanceOf('YusrilHs\\ChartjsHelper\\ChartJsLib', $pieChart);
         $this->assertInstanceOf('YusrilHs\\ChartjsHelper\\ChartJsLib', $polarChart);
         $this->assertInstanceOf('YusrilHs\\ChartjsHelper\\ChartJsLib', $radarChart);
