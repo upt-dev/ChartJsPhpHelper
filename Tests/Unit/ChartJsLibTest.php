e91fefc43210af32eca6ef128b8d6489bdd43f66
Yusril Herlian Syah <yusrilhsyah@gmail.com>
2017-10-03 00:37:59 -0400

    Modify test data

--- Tests/Unit/ChartJsLibTest.php
+++ Tests/Unit/ChartJsLibTest.php
@@ -67,7 +67,7 @@ class ChartJsLibTest extends PHPUnit_Framework_TestCase {
         $chart->useFillZero();
         $this->assertEquals($chart->usingFillZero(), true);
 
-        $dataset = $chart->createDataset('data_january', 'January');
+        $dataset = $chart->createDataset('2017', '2017');
         $this->assertEquals(count($labels), count($dataset->getData()));
 
         foreach ($dataset->getData() as $value) {
@@ -84,18 +84,69 @@ class ChartJsLibTest extends PHPUnit_Framework_TestCase {
 
     public function testCreateDataset() {
         $chart = ChartjsHelper::createChart('line');
-        $dataset = $chart->createDataset('data_january', 'January');
-        $this->assertEquals($chart->hasDataset('data_january'), true);
-        $this->assertEquals($chart->dataset('data_january'), $dataset);
+        $dataset = $chart->createDataset('2017', '2017');
+        $this->assertEquals($chart->hasDataset('2017'), true);
+        $this->assertEquals($chart->dataset('2017'), $dataset);
 
         $this->expectException('InvalidArgumentException');
-        $chart->createDataset('data_january', 'January');
+        $chart->createDataset('2017', '2017');
     }
 
     public function testInvalidDataset() {
         $chart = ChartjsHelper::createChart('line');
         
         $this->expectException('InvalidArgumentException');
-        $chart->dataset('any_data');
+        $chart->dataset('2018');
+    }
+
+    public function testGetConfig() {
+        $labels = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+        $options = array(
+            'responsive'=>true,
+            'title' => array(
+                'display' => true,
+                'text' => 'ChartjsHelper'
+            ),
+            'tooltips'=> array(
+                    'mode'=> 'index',
+                    'intersect'=> false,
+            ),
+            'hover'=> array(
+                'mode'=> 'nearest',
+                'intersect'=> true
+            ),
+            'scales'=> array(
+                'xAxes'=> array(array(
+                    'display'=> true,
+                    'scaleLabel'=> array(
+                        'display'=> true,
+                        'labelString'=> 'Month'
+                    )
+                )),
+                'yAxes'=> array(array(
+                    'display'=> true,
+                    'scaleLabel'=> array(
+                        'display'=> true,
+                        'labelString'=> 'Value'
+                    )
+                ))
+            )
+        );
+        $indonesianChartData = array(rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand());
+        $englishChartData = array(rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand());
+        $chart = ChartJsHelper::createChart('line');
+        $chart->setLabels($labels);
+        $chart->setOptions($options);
+        $chart->useRainbowColor();
+        $indonesianDataset = $chart->createDataset('indonesian', 'Indonesian');
+        $englishDataset = $chart->createDataset('english', 'English');
+
+        $indonesianDataset->setData($indonesianChartData);
+        $englishDataset->setData($englishChartData);
+
+        $config = $chart->getConfig();
+        $this->assertEquals($config['data']['labels'], $labels);
+        $this->assertEquals(count($config['data']['datasets']), 2);
+        $this->assertEquals($config['options'], $options);
     }
 }
